86bdd9eda8b89e0288ec8687ae9d7618c92201e4
Lucy Lin <144499213+LLinNYU@users.noreply.github.com>
2025-01-09 04:37:12 +0000

    *should* be done with title_reader.py after fixing topic and date logging bugs

--- read_db.py
+++ read_db.py
@@ -1,4 +1,6 @@
 import sqlite3
+# This python file is for testing and running one-time commands
+
 
 db_path = '/workspaces/stock_evaluator/article_titles/titles.db'
 
@@ -7,3 +9,37 @@ db_path = '/workspaces/stock_evaluator/article_titles/titles.db'
 conn = sqlite3.connect(db_path)
 # sql execution object
 cursor = conn.cursor()
+
+#if SQLite returns 0 then it is false, boolean types exist outside of SQLite
+# Must remove timestamp when doing date comparisons
+# cursor.execute("""SELECT DATE(MAX(pub_date)) FROM article_titles""")
+# cursor.execute("""SELECT MAX(pub_date) FROM article_titles""")
+# print(cursor.fetchone()[0])
+
+# first entry from 01/07
+# cursor.execute('SELECT * from article_titles WHERE id < 2')
+# print(cursor.fetchall())
+
+#delete all entries from today
+# cursor.execute('''SELECT COUNT(*) FROM article_titles''')
+# print(cursor.fetchone()[0])
+
+# cursor.execute('''DELETE FROM article_titles WHERE DATE(pub_date) == '2025-01-08' ''')
+
+# cursor.execute('''SELECT COUNT(*) FROM article_titles''')
+# print(cursor.fetchone()[0])
+
+# cursor.execute("SELECT * from article_titles WHERE topic != 'business' ")
+# print(cursor.fetchall())
+
+# topic = 'health'
+topic = 'business'
+
+cursor.execute("""SELECT COALESCE(DATE(MAX(pub_date)) = DATE('now','utc'),0) FROM article_titles
+                           WHERE topic = ?""",(topic,))
+print(cursor.fetchone()[0])
+
+# conn.commit()
+conn.close()
+
+# print(last_updated_today)
--- title_reader.py
+++ title_reader.py
@@ -7,10 +7,8 @@ from datetime import datetime,timezone
 
 from bs4 import BeautifulSoup
 
-# PLEASE ADD##############
 # NEED TO:
-# (1) CLOSE CONN
-# (2) NEED TO IGNORE IF THE DATE IS ALREADY FOUND IN THE DATABASE!!!!
+# Ensure that at least one entry has been run first
 
 # dictionary of RSS feeds to read each day
 rss_feeds = {
@@ -33,8 +31,8 @@ conn = sqlite3.connect(db_path)
 # sql execution object
 cursor = conn.cursor()
 
-rss_feeds_mini = {
-    'business': 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'}
+# rss_feeds_mini = {
+#     'business': 'https://news.google.com/rss/topics/CAAqJggKIiBDQkFTRWdvSUwyMHZNRGx6TVdZU0FtVnVHZ0pWVXlnQVAB?hl=en-US&gl=US&ceid=US%3Aen'}
 
 # DB ALREADY EXISTS, SAVE SOME COMPUTE RESOURCES by commenting the below
 # need id, topic, title, publication date, source
@@ -47,7 +45,39 @@ rss_feeds_mini = {
 #                source TEXT NOT NULL
 #                )""")
 
-for topic, topic_url in rss_feeds_mini.items():
+# test: get first entry from the db to see if pub_date exists
+# cursor.execute("""SELECT id,topic,title,pub_date,source FROM article_titles WHERE id = 1""")
+# print(cursor.fetchone())
+
+# What happened here???
+# cursor.execute("""PRAGMA table_info(article_titles)""")
+# print(cursor.fetchone())
+# Conclusion: the table indeed exists 
+
+
+
+# first check if there are entries:
+# if no entries:
+    # go straight to adding entries and COMMIT()
+# if yes entries:
+    # check if it has today
+    # if has today: print 'hey you did this already'
+    # else: go straight adding entries 
+def read_items(items):
+    for item in items: 
+        # only care about today's news for consistency - everything in GMT time
+        pub_date = item.find('pubDate').text
+        original_date = datetime.strptime(pub_date, "%a, %d %b %Y %H:%M:%S %Z")
+        title = item.find('title').text
+        print('Record this entry!: ', title)
+        source_name = item.find('source').text
+        sql_date = original_date.strftime("%Y-%m-%d %H:%M:%S")
+
+        # execute sanitized user inputs
+        cursor.execute("""INSERT INTO article_titles (topic,title,pub_date,source)
+                    values (?,?,?,?)""",(topic,title,sql_date,source_name))
+
+for topic, topic_url in rss_feeds.items():
     response = requests.get(topic_url)
     if response.status_code == 200:
         # parse content soup
@@ -57,33 +87,35 @@ for topic, topic_url in rss_feeds_mini.items():
         items = soup.find_all('item')
         gmt_now = datetime.now(timezone.utc).strftime("%Y-%m-%d")
 
-        # did I already run this script today and grab all the articles?
-        # if so, then don't add more
-        cursor.execute("""SELECT MAX(pub_date) latest_update FROM article_titles
-                       WHERE DATE(latest_update) = DATE('now','utc')""")
-        already_parsed_today = cursor.fetchone()
+        #count the amount of entries
+        cursor.execute("""SELECT COUNT(*) FROM article_titles""")
 
-
-        if not already_parsed_today[0]:
-            for item in items: 
-                # only care about today's news for consistency - everything in GMT time
-                pub_date = item.find('pubDate').text
-                original_date = datetime.strptime(pub_date, "%a, %d %b %Y %H:%M:%S %Z")
-
-                if (gmt_now == original_date.strftime("%Y-%m-%d")):
-                    title = item.find('title').text
-                    print('Record this entry!: ', title)
-                    source_name = item.find('source').text
-                    sql_date = original_date.strftime("%Y-%m-%d %H:%M:%S")
-
-                # execute sanitized user inputs
-                cursor.execute("""INSERT INTO article_titles (topic,title,pub_date,source)
-                            values (?,?,?,?)""",(topic,title,sql_date,source_name))
+        # if no entries added previously, thus 0 rows
+        if cursor.fetchone()[0] == 0:
+            print('no entries previously, let\'s read')
+            read_items(items)
         else:
-            print('You\'ve already saved the article titles today')   
-        # good practice
-        conn.close()
+            # table has already been written in before
+            # print('previous entries exist')
+
+            # Query: Did a specific topic for today get logged? 
+            cursor.execute("""SELECT COALESCE(DATE(MAX(pub_date)) = DATE('now','utc'),0) FROM article_titles
+                           WHERE topic = ?""",(topic,))
+            already_parsed_today = False if cursor.fetchone()[0] == 0 else True
+            
+            cursor.execute("""SELECT DATE('now','utc')""")
+            print('today\'s date in UTC:',cursor.fetchone()[0], )
+
+            #if it hasn't been parsed today, add all the entries in
+            if not already_parsed_today:
+                print('hasn\'t been parsed today')
+                read_items(items)
+            else:
+                print('You\'ve already saved the article titles today!')
+
     else: 
         print('Error :/ Status code:', response.status_code)
+conn.commit()
+conn.close()
 
-    
+        
