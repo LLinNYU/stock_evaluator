f327b819fd770b081b29e47fd01cb708c36c0067
Lucy Lin <144499213+LLinNYU@users.noreply.github.com>
2025-01-14 03:16:13 +0000

    removed duplicates

--- read_db.py
+++ read_db.py
@@ -23,7 +23,7 @@ print(cursor.fetchone()[0])
 # cursor.execute('''SELECT COUNT(*) FROM article_titles''')
 # print(cursor.fetchone()[0])
 
-cursor.execute('''DELETE FROM article_titles WHERE DATE(pub_date) = DATE('now','utc')''')
+# cursor.execute('''DELETE FROM article_titles WHERE DATE(pub_date) = DATE('now','utc')''')
 
 # cursor.execute('''SELECT COUNT(*) FROM article_titles''')
 # print(cursor.fetchone()[0])
@@ -38,7 +38,21 @@ cursor.execute('''DELETE FROM article_titles WHERE DATE(pub_date) = DATE('now','
 #                            WHERE topic = ?""",(topic,))
 # print(cursor.fetchone()[0])
 
-conn.commit()
+# cursor.execute('SELECT * FROM article_titles ORDER BY id DESC LIMIT 5')
+# print(cursor.fetchall())
+
+# cursor.execute('''CREATE TABLE new_table (
+#                id INTEGER PRIMARY KEY AUTOINCREMENT,
+#                topic TEXT NOT NULL,
+#                title TEXT NOT NULL,
+#                pub_date DATETIME NOT NULL,
+#                source TEXT NOT NULL)''')
+# cursor.execute('''INSERT INTO new_table (topic,title,pub_date,source)
+#                SELECT DISTINCT topic,title,pub_date,source FROM article_titles''')
+# cursor.execute('''DROP TABLE article_titles''')
+# cursor.execute('''ALTER TABLE new_table RENAME TO article_titles''')
+
+# conn.commit()
 conn.close()
 
 # print(last_updated_today)
