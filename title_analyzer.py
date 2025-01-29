c66619ebefca6f4f1b4d27e9ddad2c8974c9e772
Lucy Lin <144499213+LLinNYU@users.noreply.github.com>
2025-01-29 20:58:47 +0000

    created rudimentary relevant articles function

--- title_analyzer.py
+++ title_analyzer.py
@@ -7,7 +7,7 @@ import re
 # The stock code, its price, the date of the article, and the prediction is recorded.
 
 # load large pretrained English language model
-nlp = spacy.load('en_core_web_lg')
+nlp_spacy = spacy.load('en_core_web_lg')
 
 # use finbert for identifying stock-related topics/words
 finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
@@ -18,13 +18,12 @@ finance_keywords = ["acquisition", "analysis", "dividends",
                     "earnings", "economy", "forecast", 
                     "growth", "hedge", "invest", "IPO", 
                     "market", "price", "revenue", "stock", 
-                    "trading", "valuation"]
+                    "trading", "valuation", "business"]
 
 def relevant(some_title):
-    doc = nlp(some_title.lower().strip())
+    doc = nlp_spacy(some_title.lower().strip())
     org_entities = any(ent.label_ == 'ORG' for ent in doc.ents)
     has_finance_words = any(word in some_title for word in finance_keywords)
-
     return (org_entities and has_finance_words)
 
-print(relevant('Meta stocks increase'))
+print(relevant('CVS stores close'))
