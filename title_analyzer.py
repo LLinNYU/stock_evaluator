5549bd6d625efc14e3f6121f2347a08ba197e14f
Lucy Lin <144499213+LLinNYU@users.noreply.github.com>
2025-01-29 20:48:30 +0000

    still unsure how to make the relevance function

--- title_analyzer.py
+++ title_analyzer.py
@@ -1,10 +1,28 @@
 import spacy
-from transformers import pipeline
+from transformers import pipeline, BertTokenizer, BertForSequenceClassification
+import transformers
 import re
 
 # This file should determine whether an article title contains any words relevant to stocks
 # The stock code, its price, the date of the article, and the prediction is recorded.
-# check if any 
 
-#
-nlp = spacy.load('en_core_web_sm')
+# load large pretrained English language model
+nlp = spacy.load('en_core_web_lg')
+
+# use finbert for identifying stock-related topics/words
+finbert = BertForSequenceClassification.from_pretrained('yiyanghkust/finbert-tone',num_labels=3)
+tokenizer = BertTokenizer.from_pretrained('yiyanghkust/finbert-tone')
+nlp = pipeline("sentiment-analysis", model=finbert, tokenizer=tokenizer)
+
+finance_keywords = ["acquisition", "analysis", "dividends", 
+                    "earnings", "economy", "forecast", 
+                    "growth", "hedge", "invest", "IPO", 
+                    "market", "price", "revenue", "stock", 
+                    "trading", "valuation"]
+
+def relevant(some_title):
+    doc = nlp(some_title.lower().strip())
+    org_entities = any(ent.label_ == 'ORG' for ent in doc.ents)
+
+    return (org_entities and finance_keywords)
+
